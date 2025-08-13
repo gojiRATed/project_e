df12d0a540fbd6507a93be22462f2b42cf524334
dhzid-group <dhzid.group@gmail.com>
2025-08-13 23:03:04 +0700

    Step one after installation
    Signed-off-by: dhzid-group <dhzid.group@gmail.com>

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    Role::create(['name' => 'superadmin']);
+    Role::create(['name' => 'tata usaha']);
+    Role::create(['name' => 'kantin']);
+    Role::create(['name' => 'koperasi']);
+    Role::create(['name' => 'murid']);
+  }
+}
